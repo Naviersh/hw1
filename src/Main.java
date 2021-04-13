c81759ec65fab8a52160e76811c57b2eb34d25c2
naviersh <naviersh@mail.ru>
2021-04-13 22:14:56 +0500

    Исправлен импорт библиотек

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 enum Car_type
 {
